2e35d005956536c56f9579482921ac7269b647d3
ajkfds <ajkfds2018@gmail.com>
2023-05-25 06:04:47 +0900

    update for ajkControl namespace fix

--- NavigatePanel/CSourceFileNode.cs
+++ NavigatePanel/CSourceFileNode.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using ajkControls.Primitive;
 
 namespace pluginC.NavigatePanel
 {
@@ -27,10 +28,10 @@ namespace pluginC.NavigatePanel
             get => FileItem.Name;
         }
 
-        private static ajkControls.IconImage icon = new ajkControls.IconImage(Properties.Resources.c);
+        private static IconImage icon = new IconImage(Properties.Resources.c);
         public override void DrawNode(Graphics graphics, int x, int y, Font font, Color color, Color backgroundColor, Color selectedColor, int lineHeight, bool selected)
         {
-            graphics.DrawImage(icon.GetImage(lineHeight, ajkControls.IconImage.ColorStyle.White), new Point(x, y));
+            graphics.DrawImage(icon.GetImage(lineHeight, IconImage.ColorStyle.White), new Point(x, y));
             Color bgColor = backgroundColor;
             if (selected) bgColor = selectedColor;
             System.Windows.Forms.TextRenderer.DrawText(
